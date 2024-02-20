a5d78fa1b4423a998b2caaba425ef1bc15751d33
DelftMercurians <contact@delftmercurians.nl>
2024-02-20 21:18:01 +0000

    fix: set reuse address in bridge

--- bridge/src/main.rs
+++ bridge/src/main.rs
@@ -1,5 +1,6 @@
+use socket2::{Domain, Protocol, Socket, Type};
 use std::io::{self, Write};
-use std::net::{TcpListener, UdpSocket};
+use std::net::{Ipv4Addr, SocketAddr, TcpListener, UdpSocket};
 use std::sync::{Arc, Mutex};
 use std::thread;
 
@@ -27,8 +28,17 @@ fn main() -> io::Result<()> {
     });
 
     // Start vision multicast listener
-    let vision_socket = UdpSocket::bind("0.0.0.0:10006")?;
-    vision_socket.join_multicast_v4(&"224.5.23.2".parse().unwrap(), &"0.0.0.0".parse().unwrap())?;
+    // let vision_socket = socket2::Sok::bind("0.0.0.0:10006")?;
+    // vision_socket.join_multicast_v4(&"224.5.23.2".parse().unwrap(), &"0.0.0.0".parse().unwrap())?;
+    let addr = "0.0.0.0:10006".parse::<SocketAddr>().unwrap();
+    let raw_socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
+    raw_socket.set_reuse_address(true)?;
+    let multiaddr = "224.5.23.2".parse::<Ipv4Addr>().unwrap();
+    let interface = "0.0.0.0".parse::<Ipv4Addr>().unwrap();
+    raw_socket.join_multicast_v4(&multiaddr, &interface)?;
+    raw_socket.bind(&addr.into())?;
+
+    let vision_socket: UdpSocket = raw_socket.into();
 
     let mut buf = [0u8; 2 * 1024];
     loop {
